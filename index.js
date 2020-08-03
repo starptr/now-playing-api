f1c26af711ad2040e0fd47cb59dd0c53bef2d5c8
Yuto Nishida <yuyu.nishida@gmail.com>
2020-08-02 19:06:38 -0700

    First release

--- index.js
+++ index.js
@@ -1,49 +1,111 @@
 require("dotenv").config();
-const axios = require("axios");
+const SpotifyWebApi = require("spotify-web-api-node");
 const http = require("http");
+const express = require("express");
 const NodeCache = require("node-cache");
 const cache = new NodeCache({
 	stdTTL: 1,
 });
 
+let app = express();
+
+var isTokenValid = false;
+var nowPlayingData;
+
 (async () => {
-	const client = axios.create({
-		baseURL: " https://api.spotify.com/v1/",
-		headers: {
-			Authorization: `Bearer ${process.env.SPOTIFY_OAUTH_TOKEN}`,
-		},
+	var scopes = ["user-read-private", "user-read-currently-playing", "user-read-playback-state", "user-read-email"];
+
+	var spotifyApi = new SpotifyWebApi({
+		clientId: process.env.SPOTIFY_CLIENTID,
+		clientSecret: process.env.SPOTIFY_CLIENTSECRET,
+		redirectUri: process.env.SPOTIFY_REDIRECTURI,
 	});
 
-	var data = await client
-		.get("/me/player/currently-playing")
-		.then(res => {
-			console.log(res.data);
-			return res.data;
-		})
-		.catch(err => {
-			console.error(err);
-		});
+	var authorizeURL = spotifyApi.createAuthorizeURL(scopes);
 
-	var app = http
-		.createServer(async (req, res) => {
-			res.setHeader("Content-Type", "application/json");
+	console.log(authorizeURL);
 
-			if (!cache.get("isFresh")) {
-				data = await client
-					.get("/me/player/currently-playing")
-					.then(res => {
-						console.log(res.data);
-						return res.data;
-					})
+	app.get("/now-playing", async (req, res) => {
+		if (isTokenValid) {
+			if (cache.get("isFresh")) {
+				console.log("Cache is still fresh!");
+			} else {
+				nowPlayingData = await spotifyApi
+					.getMyCurrentPlaybackState({})
+					.then(data => data.body)
 					.catch(err => {
-						console.error(err);
+						console.error("Can't get current playback:", err);
 					});
 				cache.set("isFresh", true);
-			} else {
-				console.log("Cache is still fresh!");
 			}
+			res.json(nowPlayingData);
+		} else {
+			res.json({
+				error: "Token is invalid!",
+			});
+		}
+	});
 
-			res.end(JSON.stringify(data));
-		})
-		.listen(process.env.PORT || 3000);
+	//Redirect to authorizeURL if isTokenValid is false, otherwise this is for the auth callback
+	app.get("/spotify", async (req, res) => {
+		if (isTokenValid) {
+			res.send("Token is already valid!");
+		} else {
+			if (req.query.code) {
+				let code = req.query.code;
+				console.log("Code: ", code);
+
+				await spotifyApi
+					.authorizationCodeGrant(code)
+					.then(data => {
+						spotifyApi.setAccessToken(data.body["access_token"]);
+						spotifyApi.setRefreshToken(data.body["refresh_token"]);
+						console.log("Auth successful, data:", data);
+						isTokenValid = true;
+					})
+					.catch(err => {
+						console.error("Auth code grant error:", err);
+						isTokenValid = false;
+					});
+
+				await spotifyApi
+					.getMe()
+					.then(data => {
+						if (data.body.email === "yuyu.nishida@gmail.com") {
+							console.log("Expected email!");
+							res.send("Code received!");
+						} else {
+							console.log("Unexpected email!");
+							isTokenValid = false;
+							res.send("Unexpected user!");
+						}
+					})
+					.catch(err => {
+						console.error("Can't get user info:", err);
+						res.send("Can't get user info.");
+					});
+			} else {
+				res.redirect(authorizeURL);
+			}
+		}
+	});
+
+	const refresher = async () => {
+		spotifyApi
+			.refreshAccessToken()
+			.then(data => {
+				console.log("Access token refreshed!");
+				spotifyApi.setAccessToken(data.body["access_token"]);
+				isTokenValid = true;
+			})
+			.catch(err => {
+				console.error("Refresh failed. Did you log in?", err);
+				isTokenValid = false;
+			});
+	};
+	setInterval(refresher, 1000 * 60 * 5);
+
+	let server = app.listen(process.env.PORT || 3000, () => {
+		console.log("Server listening to port 3000");
+	});
 })();
